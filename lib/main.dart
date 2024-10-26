9d0245e583fff8ec5db3519a3a81d79d1bd22998
balqeeshu <balqess057@example.com>
2024-10-26 21:09:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+     return MaterialApp (
+      home:Scaffold(
+        appBar: AppBar ( title: const Text('title')),
+        body: Container(
+          color: Colors.deepPurpleAccent,
+            child:const Text('Balqees', style: TextStyle (color : Colors.purple, fontWeight: FontWeight.w400 , fontSize: 20.7 ,backgroundColor: Colors.lightBlueAccent), ) )
+      )
+    );
+  }
+
+}
