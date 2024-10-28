3ebf61eb3a762cf95b1ac6ef531d3bdaefa9efca
balqeeshu <balqess057@example.com>
2024-10-28 21:41:29 +0300

    javaScript

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-
-void main (){
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return MaterialApp (
-      home:Scaffold(
-        appBar: AppBar ( title: const Text('title')),
-        body: Container(
-          color: Colors.deepPurpleAccent,
-            child:const Text('Balqees', style: TextStyle (color : Colors.purple, fontWeight: FontWeight.w400 , fontSize: 20.7 ,backgroundColor: Colors.lightBlueAccent), ) )
-      )
-    );
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: const Text('title')),
+            body: Container(
+                color: Colors.deepPurpleAccent,
+                child: const Text(
+                  'Balqees',
+                  style: TextStyle(
+                      color: Colors.purple,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 20.7,
+                      backgroundColor: Colors.lightBlueAccent),
+                ))));
   }
-
 }
